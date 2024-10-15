c88300920197abb3a4f2c03f1c0ff3e976d9898d
Artem <moiseevartem-7@ya.ru>
2024-10-15 23:30:11 +0400

    first

---
+++ src/main/java/org/example/viberr/VibeRrApplication.java
@@ -0,0 +1,13 @@
+package org.example.viberr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeRrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeRrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/viberr/VibeRrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.viberr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeRrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
